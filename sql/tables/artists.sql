abec97a53a2c397ea226f0f79577aa7b2797e138
Paul Sarando <psarando@gmail.com>
2014-06-14 17:44:25 -0700

    Renamed artists.name_jp column to real_name.

--- sql/tables/artists.sql
+++ sql/tables/artists.sql
@@ -3,7 +3,7 @@ SET search_path = public, pg_catalog;
 CREATE TABLE artists (
   id bigint PRIMARY KEY,
   name text NOT NULL,
-  name_jp text,
+  real_name text,
   gender char(1),
   birthdate date,
   birthplace varchar,
