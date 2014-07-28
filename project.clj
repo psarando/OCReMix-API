fc876f7bb15f89d3d8e78ca3cd58f0114da1f53b
Paul Sarando <psarando@gmail.com>
2014-07-28 07:55:36 -0700

    Bumped some dependency versions.

--- project.clj
+++ project.clj
@@ -9,9 +9,8 @@
   :dependencies [[cheshire "5.3.1"]
                  [clj-http "0.9.1"]
                  [clj-time "0.7.0"]
-                 [compojure "1.1.7"]
-                 [korma "0.3.0-RC4"]
-                 [ring "1.3.0-RC1"]
+                 [korma "0.3.2"]
+                 [ring "1.3.0"]
                  [me.raynes/fs "1.4.5"]
                  [metosin/compojure-api "0.14.0"]
                  [metosin/ring-swagger-ui "2.0.17"]
@@ -19,8 +18,8 @@
                  [org.clojure/data.codec "0.1.0"]
                  [org.clojure/tools.cli "0.3.1"]
                  [org.clojure/tools.logging "0.2.6"]
-                 [postgresql "9.0-801.jdbc4"]]
-  :plugins [[lein-ring "0.8.8"]]
+                 [postgresql "9.3-1101.jdbc4"]]
+  :plugins [[lein-ring "0.8.11"]]
   :aot [org.ocremix.api.core]
   :main org.ocremix.api.core
   :ring {:handler org.ocremix.api.core/app
